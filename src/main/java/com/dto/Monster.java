7d848e6488a3d2b11a7eb3050fa588206fc4363a
Alicigdemoglu <33742088+Alicigdemoglu@users.noreply.github.com>
2021-01-29 02:19:44 -0500

    first commit

---
+++ src/main/java/com/dto/Monster.java
@@ -0,0 +1,13 @@
+package com.dto;
+
+public class Monster {
+
+    private String type;
+    private int size;
+    private int level;
+    private String name;
+    private String id;
+    public Monster() {
+
+    }
+}
---
+++ src/main/java/com/repository/MonsterRepository.java
@@ -0,0 +1,21 @@
+package com.repository;
+
+// Adapted from https://github.com/in28minutes/SpringBootForBeginners/blob/96aac7f51218090c1fa45747b9b48de5e0faabac/05.Spring-Boot-Advanced/src/main/java/com/in28minutes/springboot/jpa/UserRepository.java
+
+/*
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "monster", path = "monster")
+public interface MonsterRepository extends MongoRepository<Monster, String> {
+
+    List<Monster> getAllMonsters();
+    Monster getMonsterByID(@Param("id") String id);
+    Monster updateMonsterByID(@Param("id") String id);
+
+}
+
+ */
